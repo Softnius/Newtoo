f6746b9c52680a11010ad4bab57a91eea1ab72f9
Blaze <flightblaze@gmail.com>
2018-09-10 23:58:29 +0300

    Fixed 'if'

--- modules/dom/DOMParser.cpp
+++ modules/dom/DOMParser.cpp
@@ -5,15 +5,15 @@ namespace Newtoo
 
     Document* parseFromString(DOMString str, DOMString type)
     {
-        if(type = TextHtml or type == ApplicationXhtmlXml) // HTML парсер
+        if(type == TextHtml or type == ApplicationXhtmlXml) // HTML парсер
         {
 
         }
-        else if(type = ImageSvgXml) // SVG парсер
+        else if(type == ImageSvgXml) // SVG парсер
         {
 
         }
-        else if(type = TextXml or type == ApplicationXml) // XML парсер
+        else if(type == TextXml or type == ApplicationXml) // XML парсер
         {
 
         }
